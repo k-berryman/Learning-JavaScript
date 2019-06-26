02e61a60abb265ed89de329c463f6918e47ca9d9
Kaitlin Berryman <k.berrygirl.10@gmail.com>
2019-06-25 23:18:22 -0400

    Add files via upload

---
+++ Notes.js
@@ -0,0 +1,6 @@
+// shapes
+ellipse(200, 200, 10, 10);        	// x, y, w, h
+rect(200, 200, 10, 10);     		// x, y, w, h
+triangle(10, 10, 30, 10, 15, 15); 	// x1, y1, x2, y2, x3, y3
+point(5, 5);				// x, y
+arc(4, 4, 4, 4, 4, 4);			// x, y, w, h, start, stop
