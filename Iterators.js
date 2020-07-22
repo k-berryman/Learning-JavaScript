8bc5eda02fb2ee6c77d92d5d75f4ef098c9ce5d8
Kaitlin Berryman <k.berrygirl.10@gmail.com>
2020-07-22 18:17:26 -0400

    Create Iterators.js

---
+++ Iterators.js
@@ -0,0 +1,19 @@
+// challenge given by Codecademy
+
+// iteration methods / iterators are built-in array methods that iterate / repeat
+// iterators called on arrays manipulate elements and return values
+
+// .forEach() is an iterator
+// .forEach() runs same code for each array element
+// .forEach() takes an argument of a callback function
+//  a callback function is a function passed an argument to another function
+// .forEach() loops through the array and executes the callback function for each element
+// during each execution, the current element is passed as an argument to the callback function
+// the return value of .forEach() is always undefined
+// identifier.forEach(callbackFunction);
+// or use arrow function syntax
+// identifier.forEach(currentItem => console.log(currentItem));
+// or define beforehand
+// function printStuff(element) 
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
