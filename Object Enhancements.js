ac20a42432afbbc0914b1674594983d745000e19
Kaitlin Berryman <k.berrygirl.10@gmail.com>
2020-12-30 17:17:18 -0500

    Create Object Enhancements.js

---
+++ Object Enhancements.js
@@ -0,0 +1,14 @@
+var keyName = 'hi';
+
+var instructor = {
+  firstName: "Kait"
+}
+
+instructor[keyName + 7] = "That is my favorite!"
+
+
+instructor["key"] = "hi"
+instructor.key = "hi" // creates a 
+
+
+console.log(instructor);
