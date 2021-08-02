3eb711a3474fb9a8287c6cfc6dd82738d2da55a8
Kaitlin Berryman <k.berrygirl.10@gmail.com>
2021-08-02 19:53:54 -0400

    Update JSON-API.js

--- Express/JSON-API.js
+++ Express/JSON-API.js
@@ -4,21 +4,24 @@ app.use(express.json())
 
 const DESSERT = [
   {name: 'chocolate', qty: 12, price: 1.25},
-  {name: 'pb', qty: 24, price: .25},
+  {name: 'caramel', qty: 24, price: .25},
 ]
 
 app.get('/candies', function(req, res) {
   // res.send determines content type based on what we pass, like JSON or html
-  res.send(`${DESSERT}`)
-  res.json(DESSERT) // is res.send but always json
+  res.send(`${DESSERT}`);
+  res.json(DESSERT); // is res.send but always json
 })
 
 app.post('/candies', function(req, res) {
+  if(req.body.name.toLowerCase() === "peanut butter") {
+    res.status(403).json(msg: "peanut butter isn't allowed") // 403 error
+  }
   DESSERT.push(req.body);
-  res.json(DESSERT)
+  res.status(201).json(DESSERT); // 201 created status code
 })
 
-// json not validated for now
+// json not validated
 
 // takes a port and then runs the function
 // should be at the bottom of the file
